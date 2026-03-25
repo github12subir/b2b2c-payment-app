a457e124695372aa38c15a65c8210ea0513683d9
Subir Mondal <subir.m@stylopay.com>
2026-03-25 22:18:28 +0530

    feat: initialize multi-module microservices architecture with common-lib, API gateway, discovery server, and core services setup

---
+++ common-lib/src/test/java/com/subir/payment/common/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subir.payment.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/test/java/com/subir/payment/paymentsvc/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subir.payment.paymentsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
